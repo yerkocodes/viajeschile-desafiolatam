d3037c48cd70d88b01dfda09d915f03c96d19432
Yerko Cortes Baeza <yerkodigo@gmail.com>
2021-07-04 16:39:19 -0400

    Add SmoothScroll Property

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,20 @@
-$(document).ready(function(){
-  //TOOLTIP
-  $("[data-toggle='tooltip']").tooltip();
+$(document).ready(function () {
+
+  //Smooth Scroll
+  $(".smooth-scroll").click(function (event) {
+    event.preventDefault();
+
+    var gato = this.hash;
+
+    $("html").animate(
+      {
+	scrollTop: $(gato).offset().top - 85,
+      },
+      800
+    );
+  });
+
+//TOOLTIP
+$("[data-toggle='tooltip']").tooltip();
+
 });
